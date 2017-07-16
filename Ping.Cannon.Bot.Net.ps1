3fc530ff9dc287b6c44b6dcc2da6f4160c237652
Aaron Rosenmund <aaron.rosenmund@gmail.com>
2017-07-16 15:04:19 -0500

    3 Tools for windows - untested

---
+++ Ping.Cannon.Bot.Net.ps1
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+$name = read-host "What is the name of the domain or computer or ip"
+
+$i
+
+for($i = 0; $i -lt 100; $i++) 
+
+{
+
+Invoke-Command -computername $comptuerList -ScriptBlock {Invoke-WMIMethod win32_process create 'cmd.exe /k "ping -t 10.0.0.1"'}
+
+}
+
+
+
+out-file 
---
+++ PingSweep.ps1
@@ -0,0 +1,43 @@
+﻿
+foreach ($Computer in $ComputerName) {
+    
+    foreach ($Port in $Ports) {
+        
+        # Create a Net.Sockets.TcpClient object to use for
+        # checking for open TCP ports.
+        $Socket = New-Object Net.Sockets.TcpClient
+        
+        # Suppress error messages
+        $ErrorActionPreference = 'SilentlyContinue'
+        
+        # Try to connect
+        $Socket.Connect($Computer, $Port)
+        
+        # Make error messages visible again
+        $ErrorActionPreference = 'Continue'
+        
+        # Determine if we are connected.
+        if ($Socket.Connected) {
+            "${Computer}: Port $Port is open"
+            $Socket.Close()
+        }
+        else {
+            "${Computer}: Port $Port is closed or filtered"
+        }
+        
+        # Apparently resetting the variable between iterations is necessary.
+        $Socket.Dispose()
+        $Socket = $null
+        
+    }
+    
+}
+
+#$computer, $port = $args[0,1] # assign values to these
+$mysock = new-object net.sockets.tcpclient
+$IAsyncResult = [IAsyncResult] $mysock.BeginConnect($computer, $port, $null, $null)
+measure-command { $succ = $iasyncresult.AsyncWaitHandle.WaitOne(3000, $true) } | % totalseconds
+$succ
+$mysock.Connected
+$mysock.Close()
+$mysock.Dispose()
